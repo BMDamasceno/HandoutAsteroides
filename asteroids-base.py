b559c3899518697a1fac529ebd85164d30f1ad41
unknown <barbaramd@al.insper.edu.br>
2019-10-28 17:27:49 -0300

    Parte 4: Atirando!

--- asteroids-base.py
+++ asteroids-base.py
@@ -45,7 +45,8 @@ background_rect = background.get_rect()
 # Carrega o som 
 pygame.mixer.music.load (path.join(snd_dir, 'tgfcoder-FrozenJam-SeamlessLoop.ogg'))
 pygame.mixer.music.set_volume (0.4) #Definindo o volume
-boom_sound = pygame.mixer.Sound(path.join(snd_dir, 'expl3.wav')) 
+boom_sound = pygame.mixer.Sound(path.join(snd_dir, 'expl3.wav')) #Som da explosão
+shoot_sound = pygame.mixer.Sound(path.join(snd_dir, 'pew.wav')) #Som do tiro
 
 class Player (pygame.sprite.Sprite):
     
@@ -85,6 +86,39 @@ class Player (pygame.sprite.Sprite):
             self.rect.right = WIDTH
         if self.rect.left < 0:
             self.rect.left = 0
+            
+    def shoot (self):
+        bullet = Bullet(self.rect.center)
+        return bullet
+            
+class Bullet (pygame.sprite.Sprite):
+    
+    def __init__(self, center):
+        #Construtuor da classe pai (Sprite)
+        pygame.sprite.Sprite.__init__(self)
+        
+        #Carregando a imagem de fundo 
+        bullet_img = pygame.image.load (path.join(img_dir, "laserRed16.png"))
+        self.image = bullet_img
+        
+        #Definindo velocidade
+        self.speedy = -10 
+        
+        #Detalhes sobre o posicionamento
+        self.rect = self.image.get_rect()
+        #Posicionando o tiro com base na posicao do player e no meio dele
+        self.rect.center = center
+
+        
+    def update(self):
+        self.rect.y += self.speedy
+        #print(self.rect.y)
+        #Deletando se sair da tela
+        if self.rect.y < 0:
+            #print("sumiu")
+            self.kill()
+        
+        
     
 class Mob(pygame.sprite.Sprite):
     
@@ -112,7 +146,8 @@ class Mob(pygame.sprite.Sprite):
     def update (self):
         self.rect.x += self.speedx
         self.rect.y += self.speedy
-        
+     
+all_sprites = pygame.sprite.Group()
 
 #Cria uma nave. O construtor será chamado automaticamente
 player = Player()
@@ -120,21 +155,25 @@ player = Player()
 #Cria um grupo de sprites e adiciona a nave
 player_sprites = pygame.sprite.Group()
 player_sprites.add(player)
+all_sprites.add(player)
 
 #Cria um grupo para os meteoros
 mob_sprites = pygame.sprite.Group()
 #Grupo de mobs (8 mobs)
-for i in range (1,8):
-    mob_sprites.add(Mob()) #Adiciona aos sprites um mob
+for i in range (8):
+    mob = Mob()
+    mob_sprites.add(mob) #Adiciona aos sprites um mob
+    all_sprites.add(mob)
     
-mobs = mob_sprites
 
+bullets_sprites = pygame.sprite.Group()
 
 # Comando para evitar travamentos.
 try:
     
     # Loop principal.
     pygame.mixer.music.play(loops=-1)
+    
     running = True
     while running:
         
@@ -150,39 +189,65 @@ try:
                 
         # Verifica se apertou alguma tecla
             if event.type == pygame.KEYDOWN:
-                #Dependendo da tecla, altera a velocidade
+                #Dependendo da tecla, altera a velocidade da nave
                 if event.key == pygame.K_LEFT:
                     player.speedx = -8
                 if event.key == pygame.K_RIGHT:
                     player.speedx = 8
+                    
+                #Dispara um bala, usando a posição do player como referencia
+                if event.key == pygame.K_SPACE:
+                    bullet = player.shoot()
+                    shoot_sound.play()
+                    #print(bullet.rect.x, ",", bullet.rect.y)
+                    all_sprites.add(bullet)
+                    bullets_sprites.add(bullet)
+
+        
         #Verifica se soltou alguma tecla
             if event.type == pygame.KEYUP:
-                #Deixa a velocidade 0 novamente
+                
+                #Deixa a velocidade 0 novamente para a nave
                 if event.key == pygame.K_LEFT:
                     player.speedx = 0
                 if event.key == pygame.K_RIGHT:
                     player.speedx = 0
                 
-        #Depois de processar os eventos
+                    
+
         #Atualiza a ação do player 
-        player_sprites.update()
+        #player_sprites.update()
         #Atualiza a ação dos mobs 
-        mob_sprites.update()
+        #mob_sprites.update()
         
+        #Verifica se houve colisão entre bala e meteoro
+        hitsb = pygame.sprite.groupcollide(mob_sprites, bullets_sprites, True, True)
+        for hit in hitsb:
+            boom_sound.play()
+            m = Mob()
+            all_sprites.add(m)
+            mob_sprites.add(m)
+            
+        print (running)    
         #Verifica se houve colisão entre nave e colisão
-        hits = pygame.sprite.spritecollide(player, mobs, False, pygame.sprite.collide_circle)
+        hits = pygame.sprite.spritecollide(player, mob_sprites, False, pygame.sprite.collide_circle)
         if hits:
             #Toca o som da colisão
             boom_sound.play()
             time.sleep(1) #Precisa esperar senão fecha 
             running = False
-            
         
         # A cada loop, redesenha o fundo e os sprites
+
+        #Depois de processar os eventos
+        #Atualiza todos os sprites
+        """Não é necessário atualizar todos os grupos, pois todos os elementos estão no all_sprites"""
+        all_sprites.update()
+
         screen.fill(BLACK)
         screen.blit(background, background_rect)
-        player_sprites.draw(screen)
-        mob_sprites.draw(screen)
+        all_sprites.draw(screen)
+        #mob_sprites.draw(screen)
         
         # Depois de desenhar tudo, inverte o display.
         pygame.display.flip()
